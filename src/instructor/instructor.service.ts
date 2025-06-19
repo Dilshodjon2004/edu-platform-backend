6f5e6d1ba844fb2b1cae752e78eec4c0db81bfa9
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-06-19 16:54:47 +0500

    Instructor apply API

---
+++ src/instructor/instructor.service.ts
@@ -0,0 +1,31 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InstructorApplyDto } from './dto/instructor.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/user/user.model';
+import { Model } from 'mongoose';
+import { Instructor, InstructorDocument } from './instructor.model';
+
+@Injectable()
+export class InstructorService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Instructor.name) private instructorModel: Model<InstructorDocument>,
+  ) {}
+  async applyAsInstructor(dto: InstructorApplyDto) {
+    const { email, firstName, lastName, socialMedia } = dto;
+    let user: UserDocument | null;
+    const existUser = await this.userModel.findOne({ email });
+    user = existUser;
+    if (!existUser) {
+      const newUser = await this.userModel.create({ ...dto, fullName: `${firstName} ${lastName}` });
+      user = newUser;
+    }
+    const data = { socialMedia, author: user?._id };
+    const existInstructor = await this.instructorModel.findOne({ author: user?._id });
+    if (existInstructor)
+      throw new BadRequestException('Instructor with this email already exists in our system.');
+
+    const newInstructor = await this.instructorModel.create(data);
+    return newInstructor;
+  }
+}
