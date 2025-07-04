1896bc04ea1b88ae9691fb60ead9163379b6d29d
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-07-04 11:11:35 +0500

    Main page provider

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -60,6 +60,7 @@ export class CourseService {
       .exec();
 
     return courses.map(course => this.getSpecificFieldCourse(course));
+    // return courses;
   }
 
   getSpecificFieldCourse(course: CourseDocument) {
